220bed8aa64cc924bcb6000e494c2613e570ccec
Matthew Arthur <virtuousvolgin@gmail.com>
2022-09-14 20:04:48 +0100

    First commit

---
+++ models/orders_to_fulfill.py
@@ -0,0 +1,10 @@
+from models.order import Order
+
+order_1 = Order('Lara Croft', '2nd September', 1, 'Tomb Raider')
+order_2 = Order('Nathan Drake', '26th May', 3, 'Uncharted')
+order_3 = Order('Cloud Strife', '15th October', 9, 'Final Fantasy VII')
+order_4 = Order('David Hayter', '4th December', 69, 'Metal Gear Solid')
+order_5 = Order('Neo Cortex', '1st February', 7, 'Crash Bandicoot')
+
+
+orders = [order_1, order_2, order_3, order_4, order_5]
